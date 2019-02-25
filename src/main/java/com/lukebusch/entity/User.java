ded7060b27d1e99981a1181d4d6f0a808a3f6f97
Luke Busch <lmbusch@madisoncollege.edu>
2019-02-24 18:43:47 -0600

    create preliminary DAO and entities

--- src/main/java/com/lukebusch/entity/User.java
+++ src/main/java/com/lukebusch/entity/User.java
@@ -1,5 +1,161 @@
 package com.lukebusch.entity;
 
+import java.time.LocalDate;
+
+/**
+ * The type User.
+ */
 public class User {
 
+    private int id;
+    private String userName;
+    private String firstName;
+    private String lastName;
+    private String zipCode;
+    private LocalDate birthDate;
+
+    /**
+     * Instantiates a new User.
+     */
+    public User() {
+    }
+
+    /**
+     * Instantiates a new User.
+     *
+     * @param id        the id
+     * @param userName  the user name
+     * @param firstName the first name
+     * @param lastName  the last name
+     * @param zipCode   the zip code
+     * @param birthDate the birth date
+     */
+    public User(int id, String userName, String firstName, String lastName, String zipCode, LocalDate birthDate) {
+        this.id = id;
+        this.userName = userName;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.zipCode = zipCode;
+        this.birthDate = birthDate;
+    }
+
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Gets user name.
+     *
+     * @return the user name
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * Sets user name.
+     *
+     * @param userName the user name
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    /**
+     * Gets first name.
+     *
+     * @return the first name
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * Sets first name.
+     *
+     * @param firstName the first name
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * Gets last name.
+     *
+     * @return the last name
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * Sets last name.
+     *
+     * @param lastName the last name
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * Gets zip code.
+     *
+     * @return the zip code
+     */
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    /**
+     * Sets zip code.
+     *
+     * @param zipCode the zip code
+     */
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    /**
+     * Gets birth date.
+     *
+     * @return the birth date
+     */
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    /**
+     * Sets birth date.
+     *
+     * @param birthDate the birth date
+     */
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", zipCode='" + zipCode + '\'' +
+                ", birthDate=" + birthDate +
+                '}';
+    }
 }
---
+++ src/main/java/com/lukebusch/persistence/UserDAO.java
@@ -0,0 +1,10 @@
+package com.lukebusch.persistence;
+
+import com.lukebusch.entity.User;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.SessionFactory;
+
+public class UserDAO {
+
+}
