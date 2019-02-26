50fbd7f2660257809e2efc5f1cfc141a16822d17
Luke Busch <lmbusch@madisoncollege.edu>
2019-02-25 20:15:21 -0600

    Set up test db tools

---
+++ src/test/resources/cleandb.sql
@@ -0,0 +1,3 @@
+DELETE FROM users;
+INSERT INTO users VALUES (1, "nelly-o", "password", "Neils", "Bohr", "11122", "1885-07-10"), (2, "BigAl", "password", "Albert", "Einstein", "11223", "1879-03-14"), (3, "Brauny", "password", "Wernher", "von Braun", "12233", "1912-03-23"), (4, "uncertain", "password", "Werner-Karl", "Heisenberg", "12334", "1901-12-05");
+INSERT INTO batches VALUES (1, "", "", "", "", "", "", "");
