c8c268a0d4ae5085c2a1de80a32aa422b18758ba
Luke Busch <lmbusch@madisoncollege.edu>
2019-02-25 19:18:25 -0600

    latest version for troubleshooting

--- src/main/java/com/lukebusch/persistence/UserDAO.java
+++ src/main/java/com/lukebusch/persistence/UserDAO.java
@@ -4,7 +4,12 @@ import com.lukebusch.entity.User;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.hibernate.SessionFactory;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
+/**
+ * performs CRUD operations for the user entity
+ */
 public class UserDAO {
 
 }
