a8e6e65ae63459bcd4983f6a20c63d813077db0b
Luke Busch <lmbusch@madisoncollege.edu>
2019-04-08 12:18:17 -0500

    add more info to delete batch servlet

--- src/main/java/com/lukebusch/controller/deleteBatch.java
+++ src/main/java/com/lukebusch/controller/deleteBatch.java
@@ -11,9 +11,23 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet(name = "deleteBatch", urlPatterns = "/deleteBatch")
+/**
+ * A servlet to handle a delete request
+ *
+ * @author lbusch
+ */
 public class deleteBatch extends HttpServlet {
     // just a logger, nothing to see here...
     private final Logger logger = LogManager.getLogger(this.getClass());
+
+    /**
+     * Handles POST requests
+     *
+     * @param request the request
+     * @param response the response
+     * @throws ServletException if there is an exception, this contains the information
+     * @throws IOException if there is an exception, this contains the needed information
+     */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         // stuff to delete the selected batch here
