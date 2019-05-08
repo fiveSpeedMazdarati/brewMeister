25cfbb902462e15fc53e813b5aea6718ae758e5d
Luke Busch <lmbusch@madisoncollege.edu>
2019-05-08 14:22:35 -0500

    create properties file and loader for web service data

---
+++ src/main/java/com/lukebusch/util/PropertiesLoader.java
@@ -0,0 +1,34 @@
+package com.lukebusch.util;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertiesLoader {
+
+    private final Logger logger = LogManager.getLogger(this.getClass());
+
+    Properties properties;
+
+    /**
+     * Gets the properties for the webservices from the properties file
+     * @return a Properties object with the web services' API information
+     */
+    public Properties loadWebserviceProperties() {
+
+        properties = new Properties();
+
+        try {
+            properties.load (this.getClass().getResourceAsStream("/webservices.properties"));
+        } catch (IOException ioe) {
+            logger.error("PropertiesLoader.loadWebserviceProperties()...Cannot load the properties file");
+            logger.error(ioe);
+        } catch (Exception e) {
+            logger.error("PropertiesLoader.loadWebserviceProperties()..." + e);
+        }
+        return properties;
+
+    }
+}
