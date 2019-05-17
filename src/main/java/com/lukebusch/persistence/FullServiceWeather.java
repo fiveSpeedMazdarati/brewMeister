03829da52b90d4d609e09c1989be93af4a23f9eb
Luke Busch <lmbusch@madisoncollege.edu>
2019-05-17 11:51:35 -0500

    create weather helper

--- src/main/java/com/lukebusch/persistence/FullServiceWeather.java
+++ src/main/java/com/lukebusch/persistence/FullServiceWeather.java
@@ -1,5 +1,6 @@
 package com.lukebusch.persistence;
 
+import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lukebusch.util.DarkSkyWeatherClient;
 import com.lukebusch.weather.DataItem;
@@ -24,8 +25,6 @@ public class FullServiceWeather {
     ZipCodeClient zipCodeClient;
     DarkSkyWeatherClient weatherClient;
     String zipCode;
-    String latitude;
-    String longitude;
     String timestamp;
 
 
@@ -48,22 +47,6 @@ public class FullServiceWeather {
         this.zipCode = zipCode;
     }
 
-    public String getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(String latitude) {
-        this.latitude = latitude;
-    }
-
-    public String getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(String longitude) {
-        this.longitude = longitude;
-    }
-
     public String getTimestamp() {
         return timestamp;
     }
@@ -72,18 +55,27 @@ public class FullServiceWeather {
         this.timestamp = timestamp;
     }
 
-    public FullServiceWeather(String zipCode, String latitude, String longitude, String timestamp) {
+    public FullServiceWeather(String zipCode, String timestamp) {
         this();
 
         this.zipCode = zipCode;
-        this.latitude = latitude;
-        this.longitude = longitude;
         this.timestamp = timestamp;
     }
 
-    protected Map getLocationFromZip() {
+    public Map<String, String> getWeatherFromZip(String zipCode) {
 
-        Map<String, Double> results = new HashMap<>();
+        Map<String, String> weatherData;
+
+        Map<String, String> latLon = getLocationFromZip(zipCode);
+
+        weatherData = getWeatherForLatLon(latLon.get("latitude"), latLon.get("longitude"));
+
+        return weatherData;
+    }
+
+    public Map<String, String> getLocationFromZip(String zipCode) {
+
+        Map<String, String> results = new HashMap<>();
 
         String zipJson = zipCodeClient.getInfoForZip(zipCode);
         ObjectMapper mapper = new ObjectMapper();
@@ -91,43 +83,68 @@ public class FullServiceWeather {
 
             Response response = mapper.readValue(zipJson, Response.class);
 
-            results.put("latitude",response.getLat());
-            results.put("longitude", response.getLng());
+            results.put("latitude", String.valueOf(response.getLat()));
+            results.put("longitude", String.valueOf(response.getLng()));
 
         } catch (IOException e) {
 
             logger.error("There was a problem parsing the json from the Zip Code service");
         }
 
-        return new HashMap<String, String>();
+        return results;
     }
+    /**
+     * gets the weather based on the given information
+     *
+     * @param zipCode the zip code for the weather report
+     * @param timeStamp the time of the weather report
+     * @return the weather report with pertinent information
+     */
+    public static Map<String, String> getWeather(String zipCode, String timeStamp) {
 
+        return new HashMap<>();
+    }
     /**
      * Gets the weather report for a given latitude and longitude at a point in time
      * @return a map of the weather report containing key information
      */
-    protected Map<String, String> getWeatherForLatLon() {
+    protected Map<String, String> getWeatherForLatLon(String latitude, String longitude) {
 
-        Map<String, String> weatherReport = new HashMap<String, String>();
         ObjectMapper mapper = new ObjectMapper();
-
+        //at worst case the method returns an empty map
+        Map<String, String> weatherReport = new HashMap<>();
         String weatherJson = weatherClient.getWeatherData(latitude, longitude, timestamp);
 
         try {
+
             com.lukebusch.weather.Response response = mapper.readValue(weatherJson, com.lukebusch.weather.Response.class);
             List<DataItem> dataItems = response.getDaily().getData();
             DataItem dataItem = dataItems.get(0);
 
-            //TODO: add the important stuff from the weather report here
-            //        HighTemp, LowTemp, Humidity, Atmospheric Pressure, Pollen Count (if available)
+            weatherReport = buildWeatherReport(dataItem, weatherReport);
 
-            weatherReport.put("pressure", String.valueOf(dataItem.getPressure()));
 
-        } catch (IOException e) {
-            logger.error("There was a problem parsing the weather data from the API response");
+        } catch(JsonParseException exception) {
+            logger.error("There was a problem parsing the JSON from the API");
+        }
+        catch (IOException e) {
+            logger.error("There was a problem with the weather data API");
         }
 
         return weatherReport;
 
+    }
+
+    private Map<String, String> buildWeatherReport(DataItem dataItem, Map<String, String> weatherReport) {
+
+        // add the information from the DataItem to the weatherReport map
+        weatherReport.put("pressure", String.valueOf(dataItem.getPressure()));
+        weatherReport.put("summary", dataItem.getSummary());
+        weatherReport.put("icon", dataItem.getIcon());
+        weatherReport.put("tempHigh", String.valueOf(dataItem.getTemperatureHigh()));
+        weatherReport.put("tempLow", String.valueOf(dataItem.getTemperatureLow()));
+        weatherReport.put("humidity", String.valueOf(dataItem.getHumidity()));
+
+        return weatherReport;
     }
 }
